f776402ab5a56a548b80e6b9d2b5a81add8f89f5
Danny Leshem <dleshem@gmail.com>
2015-11-04 16:07:10 +0200

    Initial implementation.

--- test/FacebookLocales.test.js
+++ test/FacebookLocales.test.js
@@ -6,7 +6,27 @@ import * as FacebookLocales from "../src/FacebookLocales.js"
 let should = chai.should()
 
 describe("FacebookLocales", function() {
-    it ('maps en_US to en_US', function() {
+    it ("supports the default locale: English (United States)", function() {
 		FacebookLocales.bestFacebookLocaleFor("en_US").should.equal("en_US")
+    })
+
+    it ("supports a standard locale: French (France)", function() {
+		FacebookLocales.bestFacebookLocaleFor("fr_FR").should.equal("fr_FR")
+    })
+
+    it ('supports a non-standard locale: Spanish (Latin America)', function() {
+		FacebookLocales.bestFacebookLocaleFor("es_AR").should.equal("es_LA")
+    })
+
+    it ('supports a non-standard locale: Arabic', function() {
+		FacebookLocales.bestFacebookLocaleFor("ar_EG").should.equal("ar_AR")
+    })
+
+    it ('makes best effort to return a supported Facebook locale with the same langauge as an unsupported locale', function() {
+		FacebookLocales.bestFacebookLocaleFor("fr_CG").substring(0, 2).should.equal("fr")
+    })
+
+    it ('falls back to English (United States) if all else fails', function() {
+		FacebookLocales.bestFacebookLocaleFor("xx_XX").should.equal("en_US")
     })
 })
